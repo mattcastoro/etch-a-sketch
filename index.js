0da2f750539c3947b1409c5f452ffedaaf0ac6ba
Matt Castoro <matthewjcastoro@gmail.com>
2024-06-13 16:21:22 -0600

    Add styles/flex to stylesheet and DOM Methods
    Create container for divs to be added. Styled container.
    Create pladholder DOM Methods for divs to be styled.

---
+++ index.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("div");
+
+const div = document.createElement("div");
+container.appendChild(div);
+
+const div2 = document.createElement("div");
+container.appendChild(div2);
+
+const div3 = document.createElement("div");
+container.appendChild(div3);
