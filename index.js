9bbb50d553688ad78677430fd9d3205e01f10e95
Matt Castoro <matthewjcastoro@gmail.com>
2024-06-28 11:46:32 -0600

    Create function that displays grid
    Built function that creates the rows and column divs to display a grid
    of containers.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const container = document.querySelector("div");
+const container = document.querySelector(".container");
 
 function createGrid(num) {
     for (let i = 0; i < num; i++) {
         const div = document.createElement("div");
+        div.classList.add("column");
         container.appendChild(div);
+        for (let j = 0; j < num; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            div.appendChild(cell);
+        }
     }
 }
 
-createGrid(16);
+createGrid(10);
